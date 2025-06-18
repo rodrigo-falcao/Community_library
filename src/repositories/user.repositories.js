3d6deba044ca239164ba50997db0e1c2cfcc3e44
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-06-18 16:20:37 -0300

    Feat: create user repository

---
+++ src/repositories/user.repositories.js
@@ -0,0 +1,36 @@
+import db from '../config/database.js';
+
+db.run(`
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        avatar TEXT 
+    )
+`)
+
+function createUserRepository(newUser) {
+    return new Promise((res, rej) => {
+        const { username, email, password, avatar } = newUser;
+        db.run(
+            `
+            INSERT INTO users (username, email, password, avatar)
+            VALUES (?, ?, ?, ?)
+            `,
+            [username, email, password, avatar],
+            function (err) {
+                if (err) {
+                    rej(err);
+                } else {
+                    res({message: 'User created successfully', userId: this.lastID});
+                }
+            }
+        );
+    });
+}
+
+
+export default {
+    createUserRepository
+}
