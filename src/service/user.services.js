467cbb4ba2ed2998864e3a563e5dc41033349429
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-06-19 15:19:39 -0300

    Feat: bcrypt and nodemon

--- src/service/user.services.js
+++ src/service/user.services.js
@@ -1,11 +1,16 @@
 import userRepositories from "../repositories/user.repositories.js";
+import bcrypt from "bcrypt";
 
 async function createUserService(newUser) {
     const foundUser = await userRepositories.findUserByEmailRepository(newUser.email);
-    if (foundUser) {
-        throw new Error("User already exists");
-    }
-    const user = await userRepositories.createUserRepository(newUser);
+    if (foundUser) throw new Error("User already exists");
+    const passHash = await bcrypt.hash(newUser.password, 10);
+
+    const user = await userRepositories.createUserRepository({
+        ...newUser, 
+        password: passHash
+    });
+    if (!user) throw new Error("Error creating user");
     return user;
 }
 
